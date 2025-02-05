46aa88a33f7c3e50b7f0737e0457d9d32c77d275
Peter Jones <944541960@qq.com>
2025-02-06 00:06:56 +0800

    删除冗余注释

--- FangJia(pakeged)/MainWindow.xaml.cs
+++ FangJia(pakeged)/MainWindow.xaml.cs
@@ -19,11 +19,6 @@ using Category = FangJia.Common.Category;
 
 #pragma warning disable CA1416
 
-
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace FangJia;
 
 /// <summary>
