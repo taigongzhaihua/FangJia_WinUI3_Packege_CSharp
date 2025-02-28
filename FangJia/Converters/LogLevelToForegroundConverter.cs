271ba9bbc1845beadea9ded3c42f2258796c6da0
Peter Jones <944541960@qq.com>
2025-02-28 23:49:49 +0800

    新增 日志等级颜色映射

---
+++ FangJia/Converters/LogLevelToForegroundConverter.cs
@@ -0,0 +1,32 @@
+﻿using FangJia.Helpers;
+using Microsoft.UI;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+using System;
+
+namespace FangJia.Converters;
+
+public partial class LogLevelToForegroundConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is string logLevel)
+        {
+            return logLevel.ToUpperInvariant() switch
+            {
+                "DEBUG" => new SolidColorBrush { Color = Colors.DodgerBlue }, //
+                "INFO" => new SolidColorBrush { Color = ThemeHelper.IsDarkTheme() ? Colors.Cyan : Colors.DarkCyan },// 青色
+                "WARN" => new SolidColorBrush { Color = ThemeHelper.IsDarkTheme() ? Colors.Orange : Colors.DarkOrange }, // 橙色
+                "ERROR" => new SolidColorBrush { Color = Colors.IndianRed }, // 红色
+                "FATAL" => new SolidColorBrush { Color = Colors.Red }, // 红色
+                _ => new SolidColorBrush { Color = ThemeHelper.IsDarkTheme() ? Colors.White : Colors.Black }
+            };
+        }
+        return new SolidColorBrush { Color = ThemeHelper.IsDarkTheme() ? Colors.White : Colors.Black };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
